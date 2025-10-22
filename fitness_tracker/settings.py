66be4df729f86c80d29e0d2b05259e9403518eb2
Dreamboat09 <enechimdindu@gmail.com>
2025-10-22 14:53:12 +0100

    created a signal to create profile and added endpoint for authentication

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
-    'workout',
+    'workout.apps.workoutConfig',
+
 ]
 
 MIDDLEWARE = [
--- workout/views.py
+++ workout/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, ProfileSerializer, WorkoutSerializer, ExerciseSerializer
+from rest_framework import generics, permissions
+from django.contrib.auth import login, logout, authenticate
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
